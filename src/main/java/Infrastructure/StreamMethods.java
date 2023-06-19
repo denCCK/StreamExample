b8be9b51d235783bddb4895168aa2b5fbf9dddfe
Daniil <daniil909514@gmail.com>
2023-06-19 23:16:35 +0300

    Test

--- src/main/java/Infrastructure/StreamMethods.java
+++ src/main/java/Infrastructure/StreamMethods.java
@@ -7,11 +7,11 @@ import java.util.stream.Stream;
 
 public class StreamMethods {
 
-    private static void outputArchitecture(ArrayList<Manufacturer> manufacturerArrayList) {
+    public static void outputArchitectures(ArrayList<Manufacturer> manufacturerArrayList) {
         manufacturerArrayList.stream().map(Manufacturer::getARCHITECTURE).forEach(System.out::println);
     }
 
-    private static void outputManufacturerWithLess4Symbols(ArrayList<Manufacturer> manufacturerArrayList) {
+    public static void outputManufacturerWithLess4Symbols(ArrayList<Manufacturer> manufacturerArrayList) {
         Stream<Manufacturer> list = manufacturerArrayList.stream().filter(i -> i.getNAME()
                 .replaceAll(" ", "")
                 .length() < 4);
@@ -21,7 +21,7 @@ public class StreamMethods {
         }
     }
 
-    private static void outputSortedByName(ArrayList<Manufacturer> manufacturerArrayList) {
+    public static void outputSortedByName(ArrayList<Manufacturer> manufacturerArrayList) {
         Stream<Manufacturer> list = manufacturerArrayList.stream().sorted(Comparator.comparing(Manufacturer::getNAME));
         List<Manufacturer> manufacturerList = list.toList();
         for (Manufacturer manufacturer : manufacturerList) {
@@ -29,7 +29,7 @@ public class StreamMethods {
         }
     }
 
-    private static void outputNameOfProcessors(ArrayList<Manufacturer> manufacturerArrayList) {
+    public static void outputNameOfProcessors(ArrayList<Manufacturer> manufacturerArrayList) {
         Stream<Map<String, Integer>> list = manufacturerArrayList.stream().map(Manufacturer::getCPUs);
         List<Map<String, Integer>> processors = list.toList();
         for (Map<String, Integer> processor : processors) {
@@ -39,7 +39,7 @@ public class StreamMethods {
         }
     }
 
-    private static void outputNotx86Manufacturers(ArrayList<Manufacturer> manufacturerArrayList) {
+    public static void outputNotx86Manufacturers(ArrayList<Manufacturer> manufacturerArrayList) {
         Stream<Manufacturer> list = manufacturerArrayList.stream().filter(i -> !i.getARCHITECTURE().equals("x86"));
         List<Manufacturer> manufacturerList = list.toList();
         for (Manufacturer manufacturer : manufacturerList) {
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,35 @@
+import Infrastructure.StreamMethods;
+import Models.Manufacturer;
+import Reader.XmlReader;
+
+import java.io.Console;
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        final String filePath = "input.xml";
+        XmlReader reader = new XmlReader(filePath);
+        ArrayList<Manufacturer> manufacturerArrayList = reader.read();
+
+        System.out.println("Вывод архитектур производителей");
+        StreamMethods.outputArchitectures(manufacturerArrayList);
+        System.out.println();
+
+        System.out.println("Вывод всех производителей имена которых содержат меньше 4 символов");
+        StreamMethods.outputManufacturerWithLess4Symbols(manufacturerArrayList);
+        System.out.println();
+
+        System.out.println("Отсортированный вывод всех названий производителей");
+        StreamMethods.outputSortedByName(manufacturerArrayList);
+        System.out.println();
+
+        System.out.println("Вывод всех названий процессоров");
+        StreamMethods.outputNameOfProcessors(manufacturerArrayList);
+        System.out.println();
+
+        System.out.println("Вывод всех производителей не выпускающих x86 процессоров");
+        StreamMethods.outputNotx86Manufacturers(manufacturerArrayList);
+        System.out.println();
     }
 }
